858c05c8bd69595bec6827b90fc4651a0d95d0ab
Adam Dren <adamdren@gmail.com>
2022-11-12 12:11:20 +1100

    created seed file for DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.create(
+  [
+    { name: "Society", address: "80 Collins St, Melbourne", phone_number: "03 8618 8900", category: "italian" },
+    { name: "Yakimono", address: "80 Collins St, Melbourne", phone_number: "03 8616 7900", category: "japanese" },
+    { name: "Chin Chin", address: "125 Flinders Ln, Melbourne", phone_number: "03 8663 2000", category: "chinese" },
+    { name: "Kisumé", address: "175 Flinders Ln", phone_number: "03 9671 4444", category: "japanese" },
+    { name: "Hawker Hall", address: "98 Chapel St, Windsor", phone_number: "03 9960 9790", category: "chinese" },
+  ]
+)
